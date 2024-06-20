05867e40276729afc55ac2525d30119d8227041f
CJ445 <cyrilxjacob445@gmail.com>
2024-06-20 13:31:13 +0530

    Basic functionality achieved. will update UI soon

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,85 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int n){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$n.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
-        ),
+            child: Column(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red, // Set the button color to red
+                  ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: null,
+                ),
+
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange, // Set the button color to red
+                  ),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: null,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow, // Set the button color to red
+                  ),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: null,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: null,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal,
+                  ),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: null,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: null,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                  ),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: null,
+                ),
+              ],
+            )),
       ),
     );
   }
