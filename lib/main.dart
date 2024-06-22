e20d235155da796358bb26f7a777ae882f2c974e
CJ445 <cyrilxjacob445@gmail.com>
2024-06-22 13:41:24 +0530

    Made the UI better. Will work on improving code readability now

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-
-  void playSound(int n){
+  void playSound(int n) {
     final player = AudioPlayer();
     player.play(AssetSource('note$n.wav'));
   }
@@ -14,75 +13,111 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
             child: Column(
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red, // Set the button color to red
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: null,
+                  backgroundColor: Colors.red,
                 ),
-
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orange, // Set the button color to red
+                onPressed: () {
+                  playSound(1);
+                },
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: null,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.yellow, // Set the button color to red
+                onPressed: () {
+                  playSound(2);
+                },
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: null,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
+                onPressed: () {
+                  playSound(3);
+                },
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: null,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal,
+                onPressed: () {
+                  playSound(4);
+                },
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: null,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
+                onPressed: () {
+                  playSound(5);
+                },
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: null,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.purple,
+                onPressed: () {
+                  playSound(6);
+                },
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: null,
                 ),
-              ],
-            )),
+                onPressed: () {
+                  playSound(7);
+                },
+                child: null,
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
